71473ddeb79dcbea5d08dc8f1751abed3c12f839
RYA234 <mryuabc@gmail.com>
2023-05-01 09:55:28 +0900

    Section2 20 Add body 3/5 検索機能を実装　検索結果は未だ

--- src/components/HomeSearch.jsx
+++ src/components/HomeSearch.jsx
@@ -1,17 +1,29 @@
+"use client"
 import {AiOutlineSearch} from "react-icons/ai";
 import {BsFillMicFill} from "react-icons/bs";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 
 export default function HomeSearch() {  
+    const router = useRouter();
+    const[input, setInput] = useState("");
+    
+    function handleSubmit(e){
+        e.preventDefault();
+        if(!input.trim()) return;
+        router.push(`/search/web?_searchTerm=${input}`);
+    }
     return (
     <>
-        <form className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl">
+        <form onSubmit={handleSubmit} className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl">
             <AiOutlineSearch className="text-xl text-gray-500 mr-3" />
-            <input type="text" className="flex-grow focus:outline-none"/>
+            <input type="text" className="flex-grow focus:outline-none" onChange={(e) => setInput(e.target.value)} value={input}/>
             <BsFillMicFill className="text-lg"/>
         </form>
 
         <div className="">
-            <button className="btn">
+            <button onClick={handleSubmit}  className="btn">
                 Google Search
             </button>
         </div>
