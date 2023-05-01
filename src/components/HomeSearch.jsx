d5de13d70d379b1104584ef74ce8f48bcdd4ec17
RYA234 <mryuabc@gmail.com>
2023-05-01 10:36:26 +0900

    Section2 20 Add body 4/5 I`m Feeling Luckyを追加

--- src/components/HomeSearch.jsx
+++ src/components/HomeSearch.jsx
@@ -14,6 +14,14 @@ export default function HomeSearch() {
         if(!input.trim()) return;
         router.push(`/search/web?_searchTerm=${input}`);
     }
+
+    async function randomSearch(){
+        const response = await fetch("https://random-word-api.herokuapp.com/word")
+            .then((res) => res.json())
+            .then((data) => data[0]);
+        if(!response) return;
+        router.push(`/search/web?searchTerm=${response}`);
+    }
     return (
     <>
         <form onSubmit={handleSubmit} className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md focus-within:shadow-md transition-shadow sm:max-w-xl lg:max-w-2xl">
@@ -22,9 +30,13 @@ export default function HomeSearch() {
             <BsFillMicFill className="text-lg"/>
         </form>
 
-        <div className="">
+        <div className="flex flex-col space-y-2 sm:space-y-0 sm:space-x-4 justify-center sm:flex-row mt-8">
             <button onClick={handleSubmit}  className="btn">
                 Google Search
+            </button>
+
+            <button  onClick={randomSearch} className="btn flex items-center justify-center disabled:opacity-80">
+                I am Feeling Lucky
             </button>
         </div>
     </>
