9ebad45a95ac1507701c894fe8594a15d4252d71
RYA234 <mryuabc@gmail.com>
2023-05-01 11:54:13 +0900

    Section2 21 add footer 1/2 フッター部分を作成

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,12 +1,16 @@
 
 import './globals.css'
 import Head from './head'
+import Footer from '@/components/Footer'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head />
-      <body>{children}</body>
+      <body>
+        {children}
+        <Footer/>
+      </body>
     </html>
   )
 }
