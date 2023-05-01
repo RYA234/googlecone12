f28717f4cf5ee8cafaa2b45f5344344a9d6f7bbf
RYA234 <mryuabc@gmail.com>
2023-05-01 17:08:59 +0900

    Section2 23 Update the search header 2/2アイコン　レイアウトが適切になるようCSSを追加

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -7,8 +7,8 @@ import SearchHeaderOptions from "./SearchHeaderOptions";
 
 export default function SearchHeader() {
   return (
-    <header className="">
-        <div className="">
+    <header className="sticky top-0 bg-white">
+        <div className="flex w-full p-6 items-center justify-between">
             <Link href={"/"}>
                 <Image width="120" height="40" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png"/>
             </Link>
@@ -16,7 +16,7 @@ export default function SearchHeader() {
                 <SearchBox />
             </div>
 
-            <div className="">
+            <div className="hidden md:inline-flex space-x-2">
                 <RiSettings3Line className="header-icon" />
                 <TbGridDots className="header-icon" />
             </div>
