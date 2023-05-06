efca74d33523693e743aa1528dcb14b9e30f6d93
RYA234 <mryuabc@gmail.com>
2023-05-06 18:10:26 +0900

    29 Handle possible errors 1/2 APIキーなどでエラーしたときの対処

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,12 +1,16 @@
-export default async function WebSearchPage({searchParams}) {
+import Link from "next/link";
+
+export default async function WebSearchPage({ searchParams }) {
   const response = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}}`
   );
 
+  if (!response.ok) {
+    throw new Error("Something went wrong");
+  }
+
   const data = await response.json();
-  console.log(data);
   const results = data.items;
-  return (
-    <>{results && results.map((result) => <h1>{result.title}</h1>)}</>
-  )
+
+  return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
 }
