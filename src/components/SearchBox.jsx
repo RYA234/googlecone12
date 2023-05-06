0870c06f8f40bc65eda3e18a4ca95ab144592674
RYA234 <mryuabc@gmail.com>
2023-05-06 13:11:32 +0900

    26.Complete the search box component UI部分を実装

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,7 +1,17 @@
+import { RxCross2 } from "react-icons/rx";
+import { BsFillMicFill } from "react-icons/bs";
+import { AiOutlineSearch } from "react-icons/ai";
 
 
 export default function SearchBox() {
   return (
-    <div>SearchBox</div>
+   <form className="flex border border-gray-200-rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center">
+      <input text="text" className="w-full focus:outline-none"/>
+
+      <RxCross2 className="text-2xl text-gray-500 cursor-pointer sm:mr-2" />
+
+      <BsFillMicFill className="hidden sm:inline-flex text-blue-500 pl-4 border-1-2 border-gray-300 mr-3" />
+      <AiOutlineSearch className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer"/>
+   </form>
   )
 }
