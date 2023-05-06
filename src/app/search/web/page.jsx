331f3e090d3efcc0ac277166f7076693a5e38cc1
RYA234 <mryuabc@gmail.com>
2023-05-06 17:09:31 +0900

    28 fetch data from google search api and shows the titles 1/2 データ(検索結果:lecture)が取得されていることをコンソール上で確認

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,4 +1,11 @@
-export default function WebSearchPage() {
+export default async function WebSearchPage() {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=lectures`
+  );
+
+  const data = await response.json();
+  console.log(data);
+
   return (
     <div>WebSearchPage</div>
   )
