b94a3e2302ef0e8c091b4a20d34822fb372fc4de
RYA234 <mryuabc@gmail.com>
2023-05-07 11:36:50 +0900

    30 Create Search result component 3/4 APIのレスポンスのHTMLを反映するために　react-parserを導入

--- src/components/WebSearchResults.jsx
+++ src/components/WebSearchResults.jsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
+import Parser from "html-react-parser";
 
 export default function WebSearchResults({results}){
    return(
-    <div>
+    <div className="w-full mx-auto px-3 pb-24 sm:pl-[5%] md:pl-[14%] lg:pl-52">
         <p className="text-gray-600 text-sm mb-5 mt-3">
             About {results.searchInformation?.formattedTotalResults}
             results ({results.searchInformation?.formattedSearchTime} seconds)
@@ -17,7 +18,7 @@ export default function WebSearchResults({results}){
                         {result.title}
                     </Link>
                     <p className="text-gray-600">
-                        {result.htmlSnippet}
+                        {Parser(result.htmlSnippet)}
                     </p>
                 </div>
             </div>
