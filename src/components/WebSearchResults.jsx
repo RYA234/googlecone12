6e31ab2295c138d77e13263d9654f845069cbe5a
RYA234 <mryuabc@gmail.com>
2023-05-07 11:27:25 +0900

    30 Create Search result component 2/4 結果コンポーネントを作成

--- src/components/WebSearchResults.jsx
+++ src/components/WebSearchResults.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function WebSearchResults({results}){
    return(
     <div>
@@ -5,6 +7,21 @@ export default function WebSearchResults({results}){
             About {results.searchInformation?.formattedTotalResults}
             results ({results.searchInformation?.formattedSearchTime} seconds)
         </p>  
+        {results.items?.map((result) => (
+            <div className="mb-8 max-w-xl" key={result.link}>
+                <div className="group flex flex-col">
+                    <Link className="text-sm truncate" href={result.link}>
+                        {result.formattedUrl}
+                    </Link>
+                    <Link className="group-hover:underline decoration-blue-800 text-xl truncate font-medium text-blue-900" href={result.link}>
+                        {result.title}
+                    </Link>
+                    <p className="text-gray-600">
+                        {result.htmlSnippet}
+                    </p>
+                </div>
+            </div>
+        ))}
     </div>
    ) 
 }
