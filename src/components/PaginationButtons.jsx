f8008cc7e2acd5b227b9ff67094b3827c505de53
RYA234 <mryuabc@gmail.com>
2023-05-07 18:01:33 +0900

    32 Add pagination component 2/3通常検索ページの前へページを実装

--- src/components/PaginationButtons.jsx
+++ src/components/PaginationButtons.jsx
@@ -2,17 +2,25 @@
 
 import Link from "next/link";
 import {usePathname,useSearchParams} from "next/navigation";
-import { BsChevronRight } from "react-icons/bs";
+import { BsChevronLeft,BsChevronRight } from "react-icons/bs";
 
 
 export default function PaginationButton(){
     const pathname = usePathname();
     const searchParams = useSearchParams();
     const searchTerm = searchParams.get("searchTerm");
-    const startIndex = +searchParams.get("satrt");
+    const startIndex = +searchParams.get("start") || 1;
 
     return(
-        <div className="">
+        <div className="text-blue-700 flex px-10 pb-4 justify-between sm:justify-start sm:space-x-44 sm:px-0">
+      {startIndex >= 10 && (
+        <Link   href={`${pathname}?searchTerm=${searchTerm}&start=${startIndex - 10}`}>
+            <div className="flex flex-col cursor-pointer items-center hover:underline">
+                <BsChevronLeft clssName="h-5" />
+                <p>Previous</p>
+            </div>
+        </Link>
+      )}
       {startIndex <= 90 && (
         <Link
           href={`${pathname}?searchTerm=${searchTerm}&start=${startIndex + 10}`}
