fa27511022c9e31cb3dc2adbb40675297988c7fb
RYA234 <mryuabc@gmail.com>
2023-05-07 13:18:39 +0900

    31 Add Image results 3/4 cssでレイアウトを整える

--- src/components/ImageSearchResults.jsx
+++ src/components/ImageSearchResults.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function ImageSearchPage({results}){
     return(
         <div className="pb-24 mt-4">
-            <div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-x-4">
                 {results.items.map((result) => (
                     <div key={result.link} className="">
                         <div className="group">
@@ -11,7 +11,7 @@ export default function ImageSearchPage({results}){
                                 <img 
                                     src={result.link} 
                                     alt={result.title} 
-                                    className=""
+                                    className="h-60 w-full object-contain"
                                 />
                             </Link>
                             <Link href={result.image.contextLink}>
