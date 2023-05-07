7c4ba8f950aa1ad2a9037cc90bb1de5c08634291
RYA234 <mryuabc@gmail.com>
2023-05-07 13:07:22 +0900

    31 Add Image results 2/4 URLとタイトルを追加

--- src/components/ImageSearchResults.jsx
+++ src/components/ImageSearchResults.jsx
@@ -2,14 +2,28 @@ import Link from "next/link";
 
 export default function ImageSearchPage({results}){
     return(
-        <div>
+        <div className="pb-24 mt-4">
             <div>
                 {results.items.map((result) => (
                     <div key={result.link} className="">
                         <div className="group">
                             <Link href={result.image.contextLink}>
-                                <img src={result.link} alt={result.title} />
+                                <img 
+                                    src={result.link} 
+                                    alt={result.title} 
+                                    className=""
+                                />
                             </Link>
+                            <Link href={result.image.contextLink}>
+                                <h2 className="">
+                                    {result.title}
+                                </h2>
+                            </Link>
+                            <Link href={result.image.contextLink}>
+                                <p className="">
+                                    {result.displayLink}
+                                </p>
+                            </Link>                            
                         </div>
                     </div>
                 ))}
