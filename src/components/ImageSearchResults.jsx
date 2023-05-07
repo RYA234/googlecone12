f648f355437a2a9a7423cfeb2cd91b948bfad670
RYA234 <mryuabc@gmail.com>
2023-05-07 12:32:48 +0900

    31 Add Image results 1/4　画像とエラー画面の実装

---
+++ src/components/ImageSearchResults.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default function ImageSearchPage({results}){
+    return(
+        <div>
+            <div>
+                {results.items.map((result) => (
+                    <div key={result.link} className="">
+                        <div className="group">
+                            <Link href={result.image.contextLink}>
+                                <img src={result.link} alt={result.title} />
+                            </Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
